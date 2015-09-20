46179c32986791d7aef9cdea430f13b0eec2a01a
Hank Ehly <hank.ehly@mulodo.com>
2015-09-21 07:01:05 +0900

    first commit

---
+++ imgnow-ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imgnow-ios
+//
+//  Created by Henry Ehly on 2015/09/21.
+//  Copyright © 2015年 Henry Ehly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ imgnow-ios/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  imgnow-ios
+//
+//  Created by Henry Ehly on 2015/09/21.
+//  Copyright © 2015年 Henry Ehly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
