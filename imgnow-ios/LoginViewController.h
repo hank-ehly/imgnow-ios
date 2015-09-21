85ff249fd99973a84a71333f45fe1b33eb5ad60b
Hank Ehly <hank.ehly@mulodo.com>
2015-09-21 21:32:52 +0900

    login vc

---
+++ imgnow-ios/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  imgnow-ios
+//
+//  Created by Henry Ehly on 2015/09/21.
+//  Copyright © 2015年 Henry Ehly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ imgnow-ios/LoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.m
+//  imgnow-ios
+//
+//  Created by Henry Ehly on 2015/09/21.
+//  Copyright © 2015年 Henry Ehly. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"blur-bg-portrait.jpg"]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
