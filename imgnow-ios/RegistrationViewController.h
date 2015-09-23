aacd56e57bc3a0253c466cfe06247684d280cada
Hank Ehly <hank.ehly@mulodo.com>
2015-09-23 10:58:50 +0900

    registration view

---
+++ imgnow-ios/RegistrationViewController.h
@@ -0,0 +1,18 @@
+//
+//  RegistrationViewController.h
+//  imgnow-ios
+//
+//  Created by Henry Ehly on 2015/09/23.
+//  Copyright © 2015年 Henry Ehly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface RegistrationViewController : ViewController
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UITextField *confirmPasswordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *submitButton;
+- (IBAction)submitRegistration:(id)sender;
+
+@end
---
+++ imgnow-ios/RegistrationViewController.m
@@ -0,0 +1,26 @@
+//
+//  RegistrationViewController.m
+//  imgnow-ios
+//
+//  Created by Henry Ehly on 2015/09/23.
+//  Copyright © 2015年 Henry Ehly. All rights reserved.
+//
+
+#import "RegistrationViewController.h"
+
+@interface RegistrationViewController ()
+
+@end
+
+@implementation RegistrationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"blur-bg-portrait.jpg"]];
+}
+
+- (void)didReceiveMemoryWarning {[super didReceiveMemoryWarning];}
+
+- (IBAction)submitRegistration:(id)sender {
+}
+@end
