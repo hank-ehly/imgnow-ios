3b38b9ac092d980eeb2a096b7cd91c8ca860d287
Hank Ehly <hank.ehly@mulodo.com>
2015-10-04 10:56:45 +0900

    refactor logoutUser method

--- imgnow-ios/SettingsTableViewController.m
+++ imgnow-ios/SettingsTableViewController.m
@@ -16,90 +16,86 @@
 @implementation SettingsTableViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    _emailCell.detailTextLabel.text = [[NSUserDefaults standardUserDefaults] valueForKey:@"user_email"];
-    
+  [super viewDidLoad];
+  
+  _emailCell.detailTextLabel.text = [[NSUserDefaults standardUserDefaults] valueForKey:@"user_email"];
+  
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-    
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+  
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *selectedCell = [tableView cellForRowAtIndexPath:indexPath];
+  UITableViewCell *selectedCell = [tableView cellForRowAtIndexPath:indexPath];
+  
+  [tableView deselectRowAtIndexPath:indexPath animated:YES];
+  
+  if ([[[selectedCell textLabel] text] isEqualToString:@"logout"] && indexPath.row == 2) {
     
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [self confirmLogout];
     
-    if ([[[selectedCell textLabel] text] isEqualToString:@"logout"] && indexPath.row == 2) {
-        
-        [self confirmLogout];
-        
-    }
-    
+  }
+  
 }
 
 - (void) confirmLogout {
-    
-    
-    UIAlertController *ac = [UIAlertController alertControllerWithTitle:nil message:@"Are you sure you wanna logout?" preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *yes = [UIAlertAction actionWithTitle:@"yeah" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
-        [self logoutUser];
-    }];
-    UIAlertAction *no = [UIAlertAction actionWithTitle:@"no" style:UIAlertActionStyleCancel handler:nil];
-    
-    [ac addAction:yes];
-    [ac addAction:no];
-    [self presentViewController:ac animated:YES completion:nil];
-    
+  
+  
+  UIAlertController *ac = [UIAlertController alertControllerWithTitle:nil message:@"Are you sure you wanna logout?" preferredStyle:UIAlertControllerStyleAlert];
+  UIAlertAction *yes = [UIAlertAction actionWithTitle:@"yeah" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+    [self logoutUser];
+  }];
+  UIAlertAction *no = [UIAlertAction actionWithTitle:@"no" style:UIAlertActionStyleCancel handler:nil];
+  
+  [ac addAction:yes];
+  [ac addAction:no];
+  [self presentViewController:ac animated:YES completion:nil];
+  
 }
 
 - (void)logoutUser {
   
-  NSURL *url = [Api fetchUrlForApiNamedRoute:@"destroy_user_session" withResourceId:nil];
-    
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    NSURLSession *urlSession = [NSURLSession sharedSession];
-    
-    NSDictionary *loginCredentials = @{@"user":@{@"email":[[NSUserDefaults standardUserDefaults] valueForKey:@"user_email"]}};
-    NSData *requestJsonData = [NSJSONSerialization dataWithJSONObject:loginCredentials options:0 error:nil];
-    request.HTTPBody = requestJsonData;
-    
-    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    [request addValue:@"application/json" forHTTPHeaderField:@"Accept"];
-    request.HTTPMethod = @"DELETE";
-    
-    NSURLSessionDataTask *dataTask = [urlSession dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        
-        NSHTTPURLResponse *res = (NSHTTPURLResponse *)response;
-        long statusCode = [res statusCode];
-        
-        if (error) {
-            NSLog(@"%@", error);
-        }
-        
-        if (statusCode == 200) {
-            [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"user_email"];
-            [self performSegueWithIdentifier:@"logout" sender:nil];
-        }
+  NSString *uid = [[NSUserDefaults standardUserDefaults] objectForKey:@"user_email"];
+  NSMutableURLRequest *request = [Api sessionRequestForUser:uid
+                                               identifiedBy:nil
+                      isRegisteringWithPasswordConfirmation:nil];
+  
+  [Api fetchContentsOfRequest:request
+                   completion:^(NSData *data, NSURLResponse *response, NSError *error) {
+                     
+                     dispatch_async(dispatch_get_main_queue(), ^{
+                       
+                       if (error) {
+                         // handle error
+                       }
+                       
+                       switch ([Api statusCodeForResponse:response]) {
+                         case 200:
+                           [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"user_email"];
+                           [self performSegueWithIdentifier:@"logout" sender:nil];
+                           break;
+                         default:
+                           NSLog(@"Status code %ld wasn't accounted for in SettingsTableViewController logoutUser",
+                                 [Api statusCodeForResponse:response]);
+                           break;
+                       }
 
-        
-    }];
-    
-    [dataTask resume];
-
+                     });
+                   }];
+  
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 3;
+  return 3;
 }
 
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return 64.0f;
+  return 64.0f;
 }
 
 @end
