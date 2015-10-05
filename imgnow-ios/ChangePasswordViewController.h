0095afd32ebae180e4d7a6e2503f69b0e495a1d7
Hank Ehly <hank.ehly@mulodo.com>
2015-10-06 07:44:14 +0900

    change password vc

---
+++ imgnow-ios/ChangePasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChangePasswordViewController.h
+//  imgnow-ios
+//
+//  Created by Henry Ehly on 2015/10/06.
+//  Copyright © 2015年 Henry Ehly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChangePasswordViewController : UIViewController
+
+@end
---
+++ imgnow-ios/ChangePasswordViewController.m
@@ -0,0 +1,25 @@
+//
+//  ChangePasswordViewController.m
+//  imgnow-ios
+//
+//  Created by Henry Ehly on 2015/10/06.
+//  Copyright © 2015年 Henry Ehly. All rights reserved.
+//
+
+#import "ChangePasswordViewController.h"
+
+@interface ChangePasswordViewController ()
+
+@end
+
+@implementation ChangePasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
